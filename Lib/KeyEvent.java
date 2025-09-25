589509a3f5304be960eb6f842dac438fc873b1bd
J0ngja4g <phitchanan.an@ku.th>
2025-09-26 02:12:49 +0700

    Add files via upload

---
+++ Lib/KeyEvent.java
@@ -0,0 +1,20 @@
+package Lib;
+
+public class KeyEvent {
+
+    public Object getSource() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSource'");
+    }
+
+    public char getKeyChar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getKeyChar'");
+    }
+
+    public void consume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consume'");
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+import Lib.Income;
+
+public class Main {
+    public static void main(String[] args) {
+        new Income().setVisible(true);
+
+    }
+}
